4755ca242c8efdcf4f0f3321c260ec57e81946fa
TagE <58102253+VinTagE182@users.noreply.github.com>
2019-11-23 12:10:47 +0100

    Ho caricato il primo file in c

---
+++ file1.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/*
+Qui sotto ci mettiamo i prototipi di tutte le noste funzioni
+ */
+
+
+
+/*
+Qui sotto c'è il nostro main minimale
+ */
+int main(){
+
+
+  return 0;
+}
+
+
+
+
+
+
+/*
+Qui sotto le nostre innumerevoli funzioni
+ */
